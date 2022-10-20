63054d2414f33528ac9ff60c33252cd6f0c87998
Nikifor Fedorov <nikifor.fedorov@arsysop.ru>
2022-10-20 12:13:18 +0300

    Merge remote-tracking branch 'origin/main' into cpp-17-support
    added cpp17 bundles
    
    Signed-off-by: Nikifor Fedorov <nikifor.fedorov@arsysop.ru>

---
+++ bundles/ru.arsysop.loft.rgm.spec.cpp17/src/ru/arsysop/loft/rgm/spec/cpp17/Cpp17.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) ArSysOp 2018-2022
+ * 
+ * RGM Sources are publicly available only for 
+ * informational, review, analysis and consulting purposes.
+ * 
+ * Definitions, terms and conditions for using RGM Sources are stated by ArSysOp Source License version 1.0.
+ * See http://arsysop.ru/licenses/rgm/ArSysOpSourceLicense-1.0.txt
+ * 
+ * RGM Sources are provided on "as is" basis. 
+ * ArSysOp is not responsible for any damages, losses, legal prosecution 
+ * or other consequences of any sort that using RGM Sources can cause to you 
+ * (as an individual or Legal Entity), even if aware of such consequences.
+ * 
+*******************************************************************************/
+package ru.arsysop.loft.rgm.spec.cpp17;
+
+import ru.arsysop.loft.rgm.internal.spec.cpp17.Messages;
+import ru.arsysop.loft.rgm.spec.model.api.Revision;
+
+public final class Cpp17 implements Revision {
+
+	@Override
+	public String url() {
+		return "https://timsong-cpp.github.io/cppwp/n4659/"; //$NON-NLS-1$
+	}
+
+	@Override
+	public String name() {
+		return Messages.Cpp17_name;
+	}
+
+}
